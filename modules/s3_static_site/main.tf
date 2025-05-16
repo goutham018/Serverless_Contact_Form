3e969ebc805f300c22be91965acd5e3882b3487f
goutham018 <66243642+goutham018@users.noreply.github.com>
2025-05-16 10:28:43 +0530

    Updated main.tf

--- modules/s3_static_site/main.tf
+++ modules/s3_static_site/main.tf
@@ -42,3 +42,11 @@ resource "aws_s3_object" "error_html" {
   acl          = "public-read"
   content_type = "text/html"
 }
+
+resource "aws_s3_bucket_public_access_block" "allow_public" {
+  bucket                  = aws_s3_bucket.frontend.id
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
